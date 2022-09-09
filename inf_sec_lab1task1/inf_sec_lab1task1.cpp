1c6a65aa5223a9024b8b7a62a02946ea56fee342
SammySatoro <ivzhenko2014@gmail.com>
2022-09-10 00:51:35 +0300

    Добавьте файлы проекта.

---
+++ inf_sec_lab1task1/inf_sec_lab1task1.cpp
@@ -0,0 +1,77 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+string shiftLeftBy(string word, int shiftLength) {
+	string shifted = "";
+	for (int i = (shiftLength % size(word)); i < size(word); i++) {
+		shifted += word[i];
+	}
+	for (int i = 0; i < (shiftLength % size(word)); i++) {
+		shifted += word[i];
+	}
+	return shifted;
+}
+
+string shiftRightBy(string word, int shiftLength) {
+	string shifted = "";
+	for (int i = word.size() - (shiftLength % size(word)) ; i < size(word); i++) {
+		shifted += word[i];
+	}
+	for (int i = 0; i < word.size() - (shiftLength % size(word)); i++) {
+		shifted += word[i];
+	}
+	return shifted;
+}
+
+string shiftLineBy(string line, int shiftLength) {
+	int wordsSize = 0;
+	line += ' ';
+	for (int i = 0; i < line.size() - 1; i++) {
+		if (line[i] != ' ' && line[i + 1] == ' ') wordsSize++;
+	}
+	int wordIndex = 0;
+	string* words = new string[wordsSize];
+	for (int i = 0; i < line.size() - 1; i++) {
+		if (line[i] != ' ') words[wordIndex] += line[i];
+		if (line[i] != ' ' && line[i + 1] == ' ') {
+			wordIndex++;
+		}	
+	 }
+
+	string shifted = "";
+	for (int i = 0; i < wordsSize; i++) {
+		shifted += shiftRightBy(words[i], shiftLength) + ' ';
+		cout << endl << shiftRightBy(words[i], shiftLength) << endl;
+	}
+	
+	return shifted;
+}
+
+void decode(ifstream& inTextFile, ofstream& outTextFile, int shiftLength) {
+	if (!inTextFile.is_open()) {
+		return;
+	}
+	else {
+		string text = "", line = "";
+		while (!inTextFile.eof()) {
+			getline(inTextFile, line);
+			outTextFile << shiftLineBy(line, shiftLength) << endl;
+			line = "";
+		}
+		cout << endl << text << endl;
+	}
+	inTextFile.close();
+	outTextFile.close();
+}
+
+int main()
+{
+	ifstream inFile;
+	ofstream outFile;
+	inFile.open("text.txt");
+	outFile.open("outText.txt");
+	decode(inFile, outFile, 2);
+}
